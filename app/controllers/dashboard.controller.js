dda29d989198118ab6e93553a8e566eb2709b0d9
abhaybava1 <abhay.bava@orionik.com>
2025-03-03 09:48:28 +0530

    Add Repeated customer

--- app/controllers/dashboard.controller.js
+++ app/controllers/dashboard.controller.js
@@ -192,6 +192,22 @@ exports.getDashboardData = async (req, res) => {
 
             const paymentTotals = await calculatePaymentTotals({});
 
+            // Repeated Customers (Customers with more than one appointment)
+            const repeatedCustomers = await Appointment.findAll({
+                attributes: ['UserId', [db.sequelize.fn('COUNT', db.sequelize.col('id')), 'appointmentCount']],
+                group: ['UserId'],
+                having: db.sequelize.literal('COUNT(id) > 1'),
+            });
+
+            // New Customers (Customers created this month)
+            const startOfMonth = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
+            const newCustomers = await User.count({
+                where: {
+                    RoleId: customerRole.id,
+                    createdAt: { [Op.gte]: startOfMonth },
+                }
+            });
+
             data = {
                
                 // totalAdmins: await User.count({ where: { RoleId: adminRole.id } }),
@@ -212,6 +228,8 @@ exports.getDashboardData = async (req, res) => {
                 topSalonsWithDetails,
                 topBarbersWithDetails,
                 topServicesWithDetails,
+                repeatedCustomersCount: repeatedCustomers.length,
+                newCustomersCount: newCustomers,
                 revenue: {
                     online: paymentTotals.online,
                     offline: paymentTotals.offline,
@@ -307,6 +325,22 @@ exports.getDashboardData = async (req, res) => {
                 }
             });
 
+              // Repeated Customers (Customers with more than one appointment)
+              const repeatedCustomers = await Appointment.findAll({
+                attributes: ['UserId', [db.sequelize.fn('COUNT', db.sequelize.col('id')), 'appointmentCount']],
+                group: ['UserId'],
+                having: db.sequelize.literal('COUNT(id) > 1'),
+            });
+
+            // New Customers (Customers created this month)
+            const startOfMonth = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
+            const newCustomers = await User.count({
+                where: {
+                    RoleId: customerRole.id,
+                    createdAt: { [Op.gte]: startOfMonth },
+                }
+            });
+
         
             data = {
                 totalBarbers,
@@ -318,6 +352,8 @@ exports.getDashboardData = async (req, res) => {
                 completedWalkInCount,
                 canceledAppointmentsCount,
                 pendingFutureAppointmentsCount,
+                repeatedCustomersCount: repeatedCustomers.length,
+                newCustomersCount: newCustomers,
                 revenue: {
                     online: paymentTotals.online,
                     offline: paymentTotals.offline,
